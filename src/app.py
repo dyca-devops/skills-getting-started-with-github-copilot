fdb6440ab2f0087e4fe700d009a9f112a17b68e5
dyca-devops <dylan0020@gmail.com>
2026-09-23 20:23:07 +0000

    Populate activity participant lists

--- src/app.py
+++ src/app.py
@@ -25,19 +25,31 @@ activities = {
         "description": "Learn strategies and compete in chess tournaments",
         "schedule": "Fridays, 3:30 PM - 5:00 PM",
         "max_participants": 12,
-        "participants": ["michael@mergington.edu", "daniel@mergington.edu"]
+        "participants": [
+            "michael@mergington.edu",
+            "daniel@mergington.edu",
+            "grace@mergington.edu"
+        ]
     },
     "Programming Class": {
         "description": "Learn programming fundamentals and build software projects",
         "schedule": "Tuesdays and Thursdays, 3:30 PM - 4:30 PM",
         "max_participants": 20,
-        "participants": ["emma@mergington.edu", "sophia@mergington.edu"]
+        "participants": [
+            "emma@mergington.edu",
+            "sophia@mergington.edu",
+            "noah@mergington.edu"
+        ]
     },
     "Gym Class": {
         "description": "Physical education and sports activities",
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
-        "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+        "participants": [
+            "john@mergington.edu",
+            "olivia@mergington.edu",
+            "mia@mergington.edu"
+        ]
     },
     "Art Club": {
         "description": "Explore drawing, painting, and other creative projects",
